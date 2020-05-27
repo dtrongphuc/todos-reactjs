e8c939e581fb7d74528d96509e1d5c5a909dbf9e
dtrongphuc <dtrongphucdv@gmail.com>
2020-05-27 22:25:20 +0700

    css layout & add some event

--- src/App.js
+++ src/App.js
@@ -5,29 +5,80 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+		newItem: '',
         todoList: [
-            { title: "todo1", isComplete: false },
-            { title: "todo2", isComplete: false },
-            { title: "todo3", isComplete: false },
-            { title: "todo4", isComplete: false },
+            { title: "todo1", isComplete: true },
+            { title: "todo2", isComplete: true },
+            { title: "todo3", isComplete: true },
+            { title: "todo4", isComplete: true },
         ]
-  	} 
+    }
+    this.onAllItemClick = this.onAllItemClick.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onItemClick(item) {
     this.setState({
       todoList: this.state.todoList.map((i) => {
         if(i.title === item.title) {
-          i.isComplete ? i.isComplete = false : i.isComplete = true;
+          i.isComplete = !i.isComplete;
         }
         return i;
       })
     });
   }
 
+  onAllItemClick() {
+    this.setState({
+      todoList: this.state.todoList.map((i) => {
+        return i.isComplete = !i.isComplete;
+      })
+    });
+  }
+
+  onKeyUp(event) {
+	let text = event.target.value;
+    if(event.keyCode === 13) { // enter code
+      if(!text) {
+        return;
+      }
+  
+      text = text.trim();
+      if(!text) { return; }
+  
+      this.setState({
+        todoList: [
+          {title: text, isComplete: false},
+          ...this.state.todoList
+		],
+		newItem: ''
+      });
+    } else {
+		this.setState({
+			newItem: text
+		});
+	}
+  }
+
+  onChange(event) {
+	  this.setState({
+		  newItem: event.target.value
+	  });
+  }
+
   render() {
     return (
         <div className="App">
+          <div className="Header d-flex justify-content-between align-items-center">
+            <i onClick={this.onAllItemClick} className="fas fa-chevron-down"></i>
+			<input 
+				className="inputTodo" 
+				placeholder="What needs to be done?" 
+				value={this.state.newItem}
+				onChange={this.onChange}
+				onKeyUp={this.onKeyUp}></input>
+          </div>
             {
                 this.state.todoList.length > 0 &&
                 this.state.todoList.map((item, index) => {
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import classNames from 'classnames';
-
 import './TodoItem.css'
-
+import checkCircle from '../img/circle.svg';
+import checked from '../img/check.svg';
 class TodoItem extends Component {
     render() {
         const { item, onClick } = this.props;
+        let url = checkCircle;
+        if(item.isComplete) {
+            url = checked;
+        }
         return (
-            <div onClick={onClick} className={classNames('TodoItem', {
+            <div className={classNames('TodoItem', {
                 complete: item.isComplete 
             })}>
+                <img onClick={onClick} src={url} className="check-img" aria-hidden alt="this is check image"></img>
                 <p>{item.title}</p>
+                <i className="fas fa-times"></i>
             </div>
         );
     }
